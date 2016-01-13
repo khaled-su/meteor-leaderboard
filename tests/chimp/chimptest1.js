f5c01634a7e82821778ea49aa1588c69b4db3f99
Khaled Abbas <khaled3abbas@gmail.com>
2016-01-13 03:37:35 +0200

    fixes

--- tests/chimp/chimptest1.js
+++ tests/chimp/chimptest1.js
@@ -2,7 +2,7 @@ describe('Chimp Mocha', function() {
     describe('Page title', function () {
         it('should be set by the Meteor method @watch', function () {
             browser.url('http://www.google.com');
-            expect(browser.getTitle()).to.equal('Google');
+            expect(browser.getTitle()).to.equal('Googles');
         });
     });
 });
